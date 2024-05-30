b29ba0da58f55897352f0167d2489b089aee2d89
Antonello Lobianco <antonello@lobianco.org>
2024-05-30 13:41:48 +0200

    Riorganized setting loading

--- src/GenFSM.jl
+++ src/GenFSM.jl
@@ -12,6 +12,8 @@ export run
 
 include("Utilities.jl")
 
+const DIR_PATH = @__DIR__
+
 """
     run(project,scenario;force_initres_download,force_initres_ml_computation)
 
@@ -20,25 +22,19 @@ Run a scenario of GenFSM
 # Parameters
 - `project`: project or region name
 - `scenario`: name of the scenario within the project
-- `force_initres_download`: a dictionary keyed with the resource init regions with the information to what data to force download [def: empty dict]
-- `force_initres_ml_train`: a dictionary keyed with the resource init regions with the information to which parts of ML workflow to force retrain [def: empty dict]
 - `override`: a vector of pairs (keys=>new value) which eventually overrides the settings as specified in the region/scenario (e.g. for output directories)
 """
-function run(project="default",scenario="default";force_initres_download=Dict(),force_initres_ml_train=Dict(),override=[])
-    caller_path = pwd()
-    settings = REP.load_settings(project,scenario)
-    settings["caller_path"] = caller_path
-    override_nested_dict!(settings,override) # override settings from command line
+function run(project="default",scenario="default";override=[])
+    settings = REP.load_settings(project,scenario,override=override)
     region   = settings["simulation_region"]
-    resource_init_regions = settings["resource_init_regions"]
+    resources_regions = settings["res"]["regions"]
     # first get the raster map and the unistialized pixels
     raster_mask = RES.make_raster_mask(region)
-    pixels    = RES.make_pixels(settings["nft"],settings["ndc"],region["nx"],region["ny"])
+    pixels    = RES.make_pixels(settings["res"]["nft"],settings["res"]["ndc"],region["nx"],region["ny"])
     #pixels   = RES.init(ft,dc,years,region,init_regions) # return the initialized pixels
-    for (ir, reg) in enumerate(resource_init_regions)
-        #test = joinpath(@__DIR__,"..","ext","ResourceInit$(reg)Ext.jl")
-        #println(test)
-        include_expr = Meta.parse("include(joinpath(@__DIR__,\"ext\",\"ResourceInitExt_$(reg).jl\"))") # the parse happens at root level
+
+    for (ir, reg) in enumerate(resources_regions)
+        include_expr = Meta.parse("include(joinpath(DIR_PATH,\"..\",\"ext\",\"ResourceInitExt_$(reg).jl\"))") # the parse happens at root level
         eval(include_expr)
         tocall = "resources_init_$(reg)!"
 
@@ -46,15 +42,12 @@ function run(project="default",scenario="default";force_initres_download=Dict(),
         #Pkg.add(toimport)
         #expr = Meta.parse("import $toimport")
         #eval(expr)
-        force_download = get(force_initres_download,reg,"")
-        force_ml_computation = get(force_initres_ml_computation,reg,"")
-        resource_ml_model = Base.invokelatest(getfield(GenFSM, Symbol(tocall)),pixels,raster_mask,settings,force_download,force_ml_computation)
+        resource_ml_model = Base.invokelatest(getfield(GenFSM, Symbol(tocall)),pixels,raster_mask,settings)
         #RES.set_ml_model(resource_ml_model,ir)
     end
-end
 
-function test_path()
-    return pwd()
+    # ...
+    rm(settings["temp_path"]; force=true, recursive=true)
 end
 
 
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,3 +1,3 @@
 using Test, GenFSM
 
-include("Test_utilities.jl")
+#include("Test_utilities.jl")
